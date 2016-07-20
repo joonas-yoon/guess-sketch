8ecf02d600b44eb9bbfdd196736346730d1f78d9
Joona Yoon <joonas.yoon@gmail.com>
2016-07-20 01:20:47 +0000

    차례 확인 틱을 위한 타이머 추가

--- index.js
+++ index.js
@@ -41,10 +41,13 @@ var searchRoom = function (id){
 	
 app.get('/room/:room/users', function(req, res){
 	try {
-		var users = searchRoom(req.param('room')).users;
+		var room = searchRoom(req.param('room'));
 		var userlist = [];
-		for(var user in users){
-			userlist.push(user);
+		for(var user in room.users){
+			userlist.push({
+				id: user,
+				drawer: room.drawer == user
+			});
 		}
 		res.send( userlist );
 	} catch(e){
@@ -65,36 +68,68 @@ server.listen(portNumber, function(){
 	console.log('Server is listening on port %d', portNumber);
 });
 
-var getUsername = function(socket){
-	if(!socket.username){
-		socket.username = socket.id.substr(3,7);
-	}
-	return socket.username;
-};
-
-// 그림을 그릴 수 있는 지
-var isDrawer = function(socket){
-	try {
-		return getUsername(socket) == socket.room.drawer;
-	} catch(e){
-	}
-	return false;
-};
-
 // 소켓 서버 생성 및 실행
 io.set('log level', 2);
 io.sockets.on('connection', function(socket){
+	
+	var user = {
+		username: '',
+		disconnected: false,
+		isDrawer: function(){
+			if(!socket.room) return false;
+			return this.username == socket.room.drawer;
+		},
+		getUsername: function(){
+			if(!this.username){
+				this.username = socket.id.substr(3,7);
+			}
+			return this.username;
+		}
+	};
+	
+	var timer = {
+		runningTime: 60 * 1000,
+		time: 0,
+		tick: 500 /*ms*/,
+		init: function(customRunningTime){
+			if(!!customRunningTime) this.runningTime = customRunningTime;
+			this.time = this.runningTime;
+		},
+		start: function(loopEvent, endEvent){
+			this.init();
+			socket.timer_endEvent = endEvent;
+			var stop = this.stop
+			  , time = this.time
+			  , tick = this.tick;
+			socket.timer_stepper = setInterval(function(){
+				if(time <= 0){
+					stop();
+				} else {
+					loopEvent(time);
+					time -= tick;
+				}
+			}, this.tick);
+		},
+		stop: function(){
+			clearInterval(socket.timer_stepper);
+			try {
+				socket.timer_endEvent();
+			}catch(e){}
+		}
+	}
 
 	// join 이벤트
 	socket.on('join', function(data){
 		socket.join(data);
 		
-		var username = getUsername(socket);
+		var username = user.getUsername();
 		
 		var room = searchRoom(data);
 		// 해당 id의 room이 존재한다면
 		if(!!room){
 			room.users[username] = true;
+			
+			user.disconnected = false;
 			
 			socket.room = room;
 			socket.emit('render canvas', room.capturedImage);
@@ -115,45 +150,49 @@ io.sockets.on('connection', function(socket){
 	});
 	
 	socket.on('disconnect', function(){
-		
-		var username = getUsername(socket);
+		var username = user.getUsername();
+		user.disconnected = true;
 		
 		if( !socket.room ) return false;
 		
-		try {
-			delete socket.room.users[username];
-		} catch(e){
-			console.log(e);
-		}
-		
-		var users = Object.keys(socket.room.users);
-		
-		socket.leave(socket.room.id);
-		
-		if( users.length < 1 ){
-			// 모든 유저가 나갔음 (방이 비었다)
-			socket.emit('exit room');
-			delete roomArray[socket.room.id];
-			delete socket.room;
-			
-			return true;
-		}
-		
-		socket.broadcast.emit('recvMessage', username, '[' + username +']님이 퇴장하셨습니다.');
-		
-		io.sockets.in(socket.room.id).emit('update userlist');
-		
-		// 나가려는 사람이 방장이면
-		if( socket.room.owner == username ){
-			// 방장을 인계함
-			socket.room.owner = users[0];
-			socket.room.drawer = users[0];
-		}
+		setTimeout(function(){
+			if(user.disconnected){
+				try {
+					delete socket.room.users[username];
+				} catch(e){
+					console.log(e);
+				}
+				
+				var users = Object.keys(socket.room.users);
+				
+				socket.leave(socket.room.id);
+				
+				if( users.length < 1 ){
+					// 모든 유저가 나갔음 (방이 비었다)
+					socket.emit('exit room');
+					delete roomArray[socket.room.id];
+					delete socket.room;
+					
+					return true;
+				}
+				
+				socket.broadcast.emit('recvMessage', username, '[' + username +']님이 퇴장하셨습니다.');
+				
+				io.sockets.in(socket.room.id).emit('update userlist');
+				
+				// 나가려는 사람이 방장이면
+				if( socket.room.owner == username ){
+					// 방장을 인계함
+					socket.room.owner = users[0];
+					socket.room.drawer = users[0];
+				}
+			}
+		}, 10 * 1000);
 	})
 	
 	// room 이벤트
 	socket.on('send line', function(data){
-		if( isDrawer(socket) ){
+		if( user.isDrawer() ){
 			io.sockets.in(socket.room.id).emit('draw line', data);
 		}
 	});
@@ -195,10 +234,22 @@ io.sockets.on('connection', function(socket){
 	
 	////////////////// chat ///////////////////
 	socket.on('sendMessage', function(content){
-		io.sockets.in(socket.room.id).emit('recvMessage', socket.username, content);
+		if( user.isDrawer() ){
+			// 그리는 사람은 대화를 할 수 없다.
+			socket.emit('recvMessage', 'SYSTEM', '[그림을 그리는 동안은 대화를 할 수 없습니다.]');
+		} else {
+			io.sockets.in(socket.room.id).emit('recvMessage', user.username, content);
+		}
 	});
 	
-	// socket.on('game start', 
+	socket.on('start game', function(){
+		timer.init(5 * 1000);
+		timer.start(function(time){
+			console.log('진행중....' + time);
+		}, function(){
+			console.log('종료');
+		});
+	});
 	////////////////// 그림을 그릴 수 있는 권한을 넘긴다. ///////////////////
 	
 });
